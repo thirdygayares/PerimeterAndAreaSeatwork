828d7ab6c43a454b6fb3c51e88eded637ef8b3c6
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-03-20 12:25:59 +0800

    final activity

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Method that returns a Class  :");
+        System.out.println("_______________________________");
+        System.out.println("Input the dimensions of the Square( equal length and Width ): ");
+
+        System.out.print("length : ");
+        int length = sc.nextInt();
+        System.out.print("Width : ");
+        int width =sc.nextInt();
+
+        //method to print
+        print(length, width);
+
+
+    }
+
+    private static void print(int length, int width) {
+        PerimeterArea perimeterArea = new PerimeterArea(length,width);
+        System.out.println("Perimeter and Area of the square:");
+        System.out.println("Length:" + perimeterArea.getLength());
+        System.out.println("Width:" + perimeterArea.getWidth());
+        System.out.println("Perimeter:" + perimeterArea.getPerimeter());
+        System.out.println("Area:" + perimeterArea.getArea());
+
     }
 }
